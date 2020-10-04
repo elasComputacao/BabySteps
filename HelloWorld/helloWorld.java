cb0d6874f0b97962b1cfdfdc45dd038071284f52
Maysa Freire de Araújo <48970482+maysaaraujo@users.noreply.github.com>
2020-10-04 18:29:02 -0300

    Hello World em Java
    E esse foi um Hello World em Java!

--- HelloWorld/helloWorld.java
+++ HelloWorld/helloWorld.java
@@ -1,6 +1,5 @@
 public class HelloWorld {
-  public static void main(String[] args) {
-    System.out.println("hello world!")
+    public static void main(String[] args) {
+      System.out.println("Hello World");
     }
-  }
 }
