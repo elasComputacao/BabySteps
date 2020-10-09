fd14d18ddd5601a2c9393e5855dc23a07a4bef32
Gabriel Fernandes <gabriel.costa@ccc.ufcg.edu.br>
2020-10-08 21:01:40 -0300

    Add hello world in .sh

--- HelloWorld/helloWorld.java
+++ HelloWorld/helloWorld.java
@@ -1,6 +1,5 @@
 public class HelloWorld {
-  public static void main(String[] args) {
-    System.out.println("Hello World");
-  }
+    public static void main(String[] args) {
+      System.out.println("Hello World");
+    }
 }
-
