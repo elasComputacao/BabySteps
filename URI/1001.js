75a476f2cadf20fbe0dece0ebcd6ed7a2a546655
levxyca <leticiacaroline-123@hotmail.com>
2021-10-05 11:26:58 -0300

    adicionando exercícios 1001 do uri resolvido com js

---
+++ URI/1001.js
@@ -0,0 +1,9 @@
+// Questão 1001 resolvida com Javascript
+
+const input = require("fs").readFileSync("./dev/stdin", "utf8");
+
+let [A, B] = input.split("\n").map(item => parseInt(item));
+
+let X = A + B;
+
+console.log(`X = ${X}`);
