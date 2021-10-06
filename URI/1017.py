b8192af4001b7a1b48c965491ff26393c0acb41c
Karen <karen.alves@ccc.ufcg.edu.br>
2021-10-05 22:18:12 -0300

    add questao 1017 - Gasto de Combustível

---
+++ URI/1017.py
@@ -0,0 +1,7 @@
+# 12 KM/L
+# tempo gasto (h) + velocidade media (km/h)
+
+tempoGastoNaViagem = int(input()) # Em horas
+velocidadeMediaNaViagem = int(input()) # Em km/h
+
+print("{:.3f}".format(tempoGastoNaViagem*velocidadeMediaNaViagem/12))
