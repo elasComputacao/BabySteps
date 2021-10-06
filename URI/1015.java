807652a3abcb52213b63c631685574c07a73bf2b
Renaldo Silva Santino <renaldo.santino@ccc.ufcg.edu.br>
2021-10-05 23:37:24 -0300

    Questão 1015 - Distância entre dois pontos (Java)

--- URI/1015.java
+++ URI/1015.java
@@ -21,5 +21,5 @@ public class Main {
 	public static double calculaDistanciaEntrePontos(double x1, double y1, double x2, double y2) {
 		return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
 	}
-
+	
 }
