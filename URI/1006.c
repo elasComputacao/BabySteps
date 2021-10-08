e71997740295a05a3d1406c4f9db2e617504ce00
debcamila <camiladeborah72@gmail.com>
2021-10-08 15:55:28 -0300

    Resoluçao da questao 1006 do URI na linguagem c

---
+++ URI/1006.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+double main (){
+
+double a, b, c, media;
+
+scanf("%lf", &a);
+scanf("%lf", &b);
+scanf("%lf", &c);
+
+
+media = (a*2 + b*3 + c*5) / 10;
+
+printf("MEDIA = %.1lf\n", media);
+
+}
