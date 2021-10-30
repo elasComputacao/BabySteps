f8e0a5a028ffc3ac889101d3604837cad47d32ef
Edimar Chaves Jr <edimar.junior@ccc.ufcg.edu.br>
2021-10-30 12:07:25 -0300

    Added URI-1024 in Python

--- URI/1024.py
+++ URI/1024.py
@@ -1,18 +1,23 @@
 alpha = "abcdefghijklmnopqrstuvwxyz"
 
 n = int(raw_input())
+
 for i in xrange(n):
 	word = raw_input()
 	aux_word = ""
 	first_part = ""
 	second_part = ""
+
 	for j in xrange(len(word)-1, -1, -1):
 		if(word[j].lower() in alpha):
 			aux_word += chr(ord(word[j]) + 3)
 		else:
 			aux_word += word[j]
+
 	middle = (len(word)/2)
 	first_part = aux_word[0:middle]
+
 	for k in xrange((len(aux_word)/2), len(aux_word)):
 		second_part += chr(ord(aux_word[k]) -1)
+        
 	print first_part + second_part
