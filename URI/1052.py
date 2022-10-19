af821d33a06c394ad49ebc92c9ebc6d73d97e9c7
samssiq <sara.arianne.siqueira@ccc.ufcg.edu.br>
2022-10-19 12:23:13 -0300

    Questão 1052

---
+++ URI/1052.py
@@ -0,0 +1,25 @@
+x=int(input())
+if(x==1):
+    print("January")
+elif(x==2):
+    print("February")
+elif(x==3):
+    print("March")
+elif(x==4):
+    print("April")
+elif(x==5):
+    print("May")
+elif(x==6):
+    print("June")
+elif(x==7):
+    print("July")
+elif(x==8):
+    print("August")
+elif(x==9):
+    print("September")
+elif(x==10):
+    print("October")
+elif(x==11):
+    print("November")
+elif(x==12):
+    print("December")
